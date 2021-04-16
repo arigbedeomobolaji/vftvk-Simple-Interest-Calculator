a61c869a37c5702930e0536815713fdc89403244
arigbede omobolaji <arigbedeomobolaji@gmail.com>
2021-04-16 17:09:46 +0100

    final deployment

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function compute(){
     	alert("enter a positive number")
     	document.getElementById("principal").focus()
     }else {
-    document.getElementById("result").innerHTML="If you deposit <b>"+principal+"</b>,\<br\>at an interest rate of <b>"+rate+"</b>%\<br\>You will receive an amount of <b>"+amount+"</b>\<br\>in the year <b>"+year+"</b>\<br\>"
+    document.getElementById("result").innerHTML='<p class="result">If you deposit <span class="highlight">'+principal+'</span>,\<br\>at an interest rate of <span class="highlight">'+rate+'%.</span>\<br\>You will receive an amount of <span class="highlight">'+amount+'</span>\<br\>in the year <b>'+year+'</span>\<br\><p>'
 
     }
     
@@ -16,5 +16,5 @@ function compute(){
 
 function updateRate() {
     var rateVal = document.getElementById("rate").value
-    document.getElementById("rate_val").innerHTML = rateVal
+    document.getElementById("rate_val").innerHTML = rateVal + "%"
 }
