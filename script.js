513a09b6d8aa6c2c33ea8f594e7a28ca07435dd5
arigbede omobolaji <arigbedeomobolaji@gmail.com>
2021-04-16 15:27:28 +0100

    final code

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute(){
+    var principal = parseInt(document.getElementById("principal").value, 10);
+	var rate = parseFloat(document.getElementById("rate").value);
+	var years = parseInt(document.getElementById("years").value, 10);
+	var year = new Date().getFullYear() + years;
+    var amount = principal * years * rate / 100;
+    document.getElementById("result").innerHTML="If you deposit <b>"+principal+"</b>,\<br\>at an interest rate of <b>"+rate+"</b>%\<br\>You will receive an amount of <b>"+amount+"</b>,\<br\>in the year <b>"+year+"</b>\<br\>"
     
 }
-        
+
+function updateRate() {
+    var rateVal = document.getElementById("rate").value
+    document.getElementById("rate_val").innerHTML = rateVal
+}
