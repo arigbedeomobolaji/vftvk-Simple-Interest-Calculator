e700d39f371402d6ecc81f4b8e262824bde6e3a6
arigbede omobolaji <arigbedeomobolaji@gmail.com>
2021-04-16 16:17:17 +0100

    setup principal input focus

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function compute(){
     var amount = principal * years * rate / 100;
     if(principal <= 0 || !principal){
     	alert("enter a positive number")
+    	document.getElementById("principal").focus()
     }else {
     document.getElementById("result").innerHTML="If you deposit <b>"+principal+"</b>,\<br\>at an interest rate of <b>"+rate+"</b>%\<br\>You will receive an amount of <b>"+amount+"</b>\<br\>in the year <b>"+year+"</b>\<br\>"
-    	
+
     }
     
 }
